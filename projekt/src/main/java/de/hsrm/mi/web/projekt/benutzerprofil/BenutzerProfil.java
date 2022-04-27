fa7d67d85b72ebb6a438a9c06f0988cb73956e7a
Alexandra Müller <alexandra.mueller_1@student.hs-rm.de>
2022-04-27 14:20:49 +0200

    init

---
+++ projekt/src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfil.java
@@ -0,0 +1,67 @@
+package de.hsrm.mi.web.projekt.benutzerprofil;
+
+import java.time.LocalDate;
+
+public class BenutzerProfil {
+    private String name = "";
+    private LocalDate geburtsdatum = LocalDate.of(1,1,1);
+    private String adresse = "";
+    private String email = "";
+    private String lieblingsfarbe = "";
+    private String interessen = "";
+
+    //Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getGeburtsdatum() {
+        return geburtsdatum;
+    }
+
+    public void setGeburtsdatum(LocalDate geburtsdatum) {
+        this.geburtsdatum = geburtsdatum;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLieblingsfarbe() {
+        return lieblingsfarbe;
+    }
+
+    public void setLieblingsfarbe(String lieblingsfarbe) {
+        this.lieblingsfarbe = lieblingsfarbe;
+    }
+
+    public String getInteressen() {
+        return interessen;
+    }
+
+    public void setInteressen(String interessen) {
+        this.interessen = interessen;
+    }
+
+    @Override
+    public String toString() {
+        return "BenutzerProfil [adresse=" + adresse + ", email=" + email + ", geburtsdatum=" + geburtsdatum
+                + ", interessen=" + interessen + ", lieblingsfarbe=" + lieblingsfarbe + ", name=" + name + "]";
+    }  
+}
