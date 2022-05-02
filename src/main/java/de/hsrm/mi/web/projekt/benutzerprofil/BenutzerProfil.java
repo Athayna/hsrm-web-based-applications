559a4bab2741fb20e752413c7b18c381a10c5e2f
Alexandra Müller <alexandra.mueller_1@student.hs-rm.de>
2022-05-02 21:50:44 +0200

    fixes to pass tests A1 and A2

--- src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfil.java
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfil.java
@@ -2,6 +2,7 @@ package de.hsrm.mi.web.projekt.benutzerprofil;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class BenutzerProfil {
@@ -62,7 +63,18 @@ public class BenutzerProfil {
     }
 
     public List<String> getInteressenListe() {
-        List<String> interessen = Arrays.asList(this.interessen.split(", "));
+        List<String> interessen; 
+        if (this.interessen.equals(""))
+            return (interessen = Collections.emptyList());
+        interessen = Arrays.asList(this.interessen.split(","));
+        for (String e : interessen) {
+            while (e.endsWith(" ")) {
+                e = e.substring(0, e.length() - 1);
+            }
+            while (e.startsWith(" ")) {
+                e = e.substring(1, e.length());
+            }
+        }
         return interessen;
     }
 
@@ -76,11 +88,7 @@ public class BenutzerProfil {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
         result = prime * result + ((geburtsdatum == null) ? 0 : geburtsdatum.hashCode());
-        result = prime * result + ((interessen == null) ? 0 : interessen.hashCode());
-        result = prime * result + ((lieblingsfarbe == null) ? 0 : lieblingsfarbe.hashCode());
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
@@ -94,30 +102,10 @@ public class BenutzerProfil {
         if (getClass() != obj.getClass())
             return false;
         BenutzerProfil other = (BenutzerProfil) obj;
-        if (adresse == null) {
-            if (other.adresse != null)
-                return false;
-        } else if (!adresse.equals(other.adresse))
-            return false;
-        if (email == null) {
-            if (other.email != null)
-                return false;
-        } else if (!email.equals(other.email))
-            return false;
         if (geburtsdatum == null) {
             if (other.geburtsdatum != null)
                 return false;
         } else if (!geburtsdatum.equals(other.geburtsdatum))
-            return false;
-        if (interessen == null) {
-            if (other.interessen != null)
-                return false;
-        } else if (!interessen.equals(other.interessen))
-            return false;
-        if (lieblingsfarbe == null) {
-            if (other.lieblingsfarbe != null)
-                return false;
-        } else if (!lieblingsfarbe.equals(other.lieblingsfarbe))
             return false;
         if (name == null) {
             if (other.name != null)
--- src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfilController.java
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfilController.java
@@ -3,11 +3,14 @@ package de.hsrm.mi.web.projekt.benutzerprofil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class BenutzerProfilController {
+public class BenutzerprofilController {
     @GetMapping("/benutzerprofil")
-    public String benutzerprofil(Model m) {
+    public String benutzerprofil(@ModelAttribute("profil") BenutzerProfil profil, Model m) {
+        profil.setName("Alexandra Müller");
+
         return "/benutzerprofil/profilansicht";
     }
 }
