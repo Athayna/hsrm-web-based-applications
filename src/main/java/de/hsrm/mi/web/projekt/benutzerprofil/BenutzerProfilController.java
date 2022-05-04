a0d29110c16dfcb7d78e3e863fee31358d893655
Alexandra Müller <alexandra.mueller_1@student.hs-rm.de>
2022-05-04 20:13:17 +0200

    implemented POST for editor

--- src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfilController.java
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfilController.java
@@ -2,15 +2,21 @@ package de.hsrm.mi.web.projekt.benutzerprofil;
 
 import java.time.LocalDate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes(names = {"profil"})
 public class BenutzerprofilController {
+    Logger logger = LoggerFactory.getLogger(BenutzerprofilController.class);
+
     @ModelAttribute("profil")
     public void initProfil(Model m) {
         BenutzerProfil p = new BenutzerProfil();
@@ -31,5 +37,14 @@ public class BenutzerprofilController {
     @GetMapping("/benutzerprofil/bearbeiten")
     public String bearbeiten(Model m) {
         return "/benutzerprofil/profileditor";
+    }
+
+    @PostMapping("/benutzerprofil/bearbeiten")
+    public String bearbeitet(Model m, @ModelAttribute("profil") BenutzerProfil profil, BindingResult result) {
+        if (result.hasErrors()) {
+            return "benutzerprofil/profilansicht";
+        }
+        m.addAttribute("profil", profil);
+        return "redirect:/benutzerprofil";
     }
 }
